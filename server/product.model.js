317f3a3a4923e23a565f46ccf5efa4412f4eed93
Somar-sabe <119017905+Somar-sabe@users.noreply.github.com>
2023-02-10 16:08:13 +0400

    Update product.model.js

--- server/product.model.js
+++ server/product.model.js
@@ -1,21 +1,20 @@
 const mongoose = require("mongoose");
 const productSchema = mongoose.Schema({
-    category: { type: String, required: true },
-    country: { type: String, required: true },
-    company: { type: String, required: true },
-    brand: { type: String, required: true },
-    name: { type: String, required: true },
-    description: { type: String, required: true },
+    category: { type: String },
+    country: { type: String },
+    company: { type: String },
+    brand: { type: String  },
+    name: { type: String  },
     price: { type: Number },
-    capacity: { type: Number, required: true },    
+    capacity: { type: Number  },    
     image: { type: String },
-    netweight: { type: Number, required: true },
-    grossWeight: { type: Number, required: true },
-    palletSize:{ type: Number, required: true },
+    netweight: { type: Number  },
+    grossWeight: { type: Number },
+    palletSize:{ type: Number },
     bl:[{
-        code: { type: String, required: true },
-        qty: {type: Number, required: true},
-        date:{type: Date ,required:true},
+        code: { type: String  },
+        qty: {type: Number},
+        date:{type: Date },
         warehouse:{type:String}
     }]
 });
